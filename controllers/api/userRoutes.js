da05bd1ff920092bc898cc7369868cd032eb8f73
bean <bkung188@gmail.com>
2022-01-05 10:39:30 -0800

    changing userRoutes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -4,4 +4,16 @@ router.get("/", async (req, res) => {
     res.json("here")
 })
 
+router.post('/login', async (req, res) => {
+    try{
+        const userData = await User.findOne({ where: { email.req.body.email } });
+
+        if(!userData){
+            res
+                .status(400)
+                .json({message: 'Incorrect email or password, please try again!'})
+            return;
+        }
+    }
+})
 module.exports = router
