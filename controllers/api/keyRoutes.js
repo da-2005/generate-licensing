f391575e5b58ed4f4dd5e3484f340b933cf025b5
bean <bkung188@gmail.com>
2022-01-05 13:19:31 -0800

    added keyRoutes js

---
+++ controllers/api/keyRoutes.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const { Keys } = require('../../models');
+
+router.post('/' async (req, res) => {
+    try {
+        const newKey = await Keys.create({
+
+        });
+
+        res.status(200).json(newKey)
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+
+module.exports = router;
