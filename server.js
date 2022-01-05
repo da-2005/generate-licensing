2841ddbcb28540eebe771a7ccd51e26ab31a9578
Sebastian Lample <SebastianLample@gmail.com>
2022-01-05 10:17:21 -0800

    Initial Commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const hbs = exphbs.create({ });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'views/public')));
+
+app.listen(PORT, () => console.log('Now listening'));
