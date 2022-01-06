65d4fbf6cedf8c2d472dd80a7050ce27ff7a9681
bean <bkung188@gmail.com>
2022-01-06 11:30:32 -0800

    added to keyRoutes js

--- controllers/api/keyRoutes.js
+++ controllers/api/keyRoutes.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
+const req = require('express/lib/request');
 const { Key } = require('../../models');
 const withAuth = require('../../util/auth')
 
 router.post('/', withAuth, async (req,res) => {
     try {
-        const newKey = await Project.create({
+        const newKey = await Key.create({
             ...req.body,
             key: req.session.key,
         });
@@ -17,25 +18,33 @@ router.post('/', withAuth, async (req,res) => {
 
 router.delete('/:id', withAuth, async (req, res) => {
     try{
-        const projectData = await Project.destroy({
+        const keyData = await Key.destroy({
             where: {
                 id: req.params.key,
                 key: req.session.key
             },
         });
 
-        if(!projectData){
-            res.status(404).json({ message: 'No project found' })
+        if(!keyData){
+            res.status(404).json({ message: 'No key found' })
             return;
         }
 
-        res.status(200).json(projectData);
+        res.status(200).json(keyData);
     }   catch(err) {
         res.status(500).json(err);
     }
 });
 
+router.get('/:id', withAuth, async (req, res) => {
+    try{
+        const key = await Key.findByPk(req.params.key)
 
+        res.status(200).json(key);
+    } catch(err) {
+        res.status(500).json(err);
+    }
+})
 
 
 module.exports = router;
