446aa1cde2a995be0885d7f17214fdc7d0965cc5
Sebastian Lample <SebastianLample@gmail.com>
2022-01-06 13:13:01 -0800

    Projects Page

--- controllers/loggedIn/userRoutes.js
+++ controllers/loggedIn/userRoutes.js
@@ -1,11 +1,22 @@
 const router = require('express').Router();
+const withAuth = require('../../util/auth')
+const Project = require('../../models/Project')
 
 router.get("/login", (req, res) => {
     res.render('login')
 })
 
-router.get('/projects', async (req, res) => {
-    res.render('projects')
+router.get('/projects', withAuth, async (req, res) => {
+    try {
+        let usersProjects = await Project.findAll({ where: { user_id: req.session.user_id } })
+
+        res.render('projects', {
+            usersProjects
+        })
+    } catch(err) {
+        res.sendStatus(500)
+        console.log(err)
+    }   
 })
 
 module.exports = router
