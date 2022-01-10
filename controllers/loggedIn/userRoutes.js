0cb8fafa4fa60723e1e2eb00c6fd10dca9127e08
kdot <alvarezderrick0@gmail.com>
2022-01-10 12:33:14 -0800

    add keys

--- controllers/loggedIn/userRoutes.js
+++ controllers/loggedIn/userRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const withAuth = require('../../util/withAuth')
 const Project = require('../../models/Project')
-
+const Key = require('../../models/Key');
 router.get("/login", (req, res) => {
     res.render('login')
 })
@@ -14,6 +14,17 @@ router.get('/projects', withAuth, async (req, res) => {
     } catch(err) {
         res.sendStatus(500).json(err)
     }   
+})
+
+router.get('/keys:project_id', withAuth, async (req, res) => {
+    console.log('hi')
+    try {
+        let keys = await Key.findAll({where: req.params.project_id})
+        keys = keys.map(key => key.get( {plain:true} ))
+        res.render('keys', {keys, logged_in:req.session.logged_in})
+    } catch(err) {
+        res.sendStatus(500).json(err)
+    }
 })
 
 module.exports = router
